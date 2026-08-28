7364140d14fdd2bf9ee0f46d0f4452c8724dc44d
teacher9sgoo-max <teacher9sgoo@gmail.com>
2026-08-28 10:12:36 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='gijang-teacher-helper-v2';
+const CACHE_NAME='gijang-teacher-helper-v3';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.svg','./icons/icon-512.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
