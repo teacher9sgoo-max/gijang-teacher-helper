f5d5b45c013017fbe95d38e3a26fc612d7f47257
teacher9sgoo-max <teacher9sgoo@gmail.com>
2026-09-04 10:45:58 +0900

    Update dashboard-config.js

--- dashboard-config.js
+++ dashboard-config.js
@@ -6,5 +6,5 @@
       index.html을 다시 받을 필요 없이 아래 줄의 주소만 새 주소로 바꿔서
       GitHub에 올리면 바로 반영됩니다. (앞줄의 // 를 지우고 주소를 넣어주세요)
 */
-// window.GIJANG_WRITE_API_URL = 'https://script.google.com/macros/s/AKfycbxGhlh7YaFRcXYjGW0zGlAKAg9hm-lLwAC6wqvtVJSw6788oajCpNwc1DM4i8rUnClm/exec';
+// window.GIJANG_WRITE_API_URL = 'https://script.google.com/macros/s/AKfycbx6_xm4fjw7TWFcwmWOCLCSpBAPE-O5sHDHbfHFWvh6SPYF-UOsBffgerMOh682dibJ/exec';
 // window.GIJANG_TIMETABLE_SERVER_URL = '';
